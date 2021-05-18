3252ed6406b3ae1be2e8f7b5455f49a5e18f699f
vothikimyen108 <vothikinyen2k@gmail.com>
2021-05-19 00:31:13 +0700

    creat app

---
+++ Areas/Admin/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Myapp.Areas.Admin.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Myapp.MvcApplication" Language="C#" %>
